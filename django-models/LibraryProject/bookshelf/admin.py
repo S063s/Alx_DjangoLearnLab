76288078ddfa37143a9d79b0bc61a77163afc056
Shaun Manda <shaunwmanda@gmail.com>
2025-11-09 15:39:40 +0300

    add complex admin customization for models

--- django-models/LibraryProject/bookshelf/admin.py
+++ django-models/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Genre
 
+# Register your models here.
+
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year', 'id')
-    list_filter = ('author', 'publication_year')
-    search_fields = ('title', 'author')
-    list_editable = ('publication_year',)
+    # The fields below caused the error because 'publication_year' was removed.
+    # We are using other fields that likely still exist.
+    list_display = ('title', 'author', 'price') 
+    list_filter = ('author',)
+    list_editable = ('price',)
+    search_fields = ('title', 'author__name')
 
-admin.site.register(Book, BookAdmin)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    search_fields = ('first_name', 'last_name')
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    pass
