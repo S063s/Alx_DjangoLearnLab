4d7611ea1e0d8b123f0b2b21c41335ecf9019e5a
Shaun Manda <shaunwmanda@gmail.com>
2025-11-09 16:56:22 +0300

    add links to newly created templates of login, logout, and register views in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -23,4 +23,9 @@ urlpatterns = [
     path('add_book/', add_book, name='add_book'),
     path('edit_book/<int:pk>/', edit_book, name='edit_book'),
     path('delete_book/<int:pk>/', delete_book, name='delete_book'),
+
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
 ]
